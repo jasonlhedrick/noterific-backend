9e7aa472458f61e6fe7e14debd6e8f1e0e082ddf
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-13 13:54:06 -0700

    Decided to expand out pass to password to head off any confustion.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const bcrypt = require('./helpers/bcrypt');
 
 async function test() {
-    const testHash = await bcrypt.hashPass('testing');
+    const testHash = await bcrypt.hashPassword('testing');
     console.log(`testhash: ${testHash}`);
     console.log(await bcrypt.verifyHash('testing', testHash));
 }
