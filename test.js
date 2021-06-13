4d67686afeab48e10cc1636165772e629662ca36
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-13 13:55:35 -0700

    Changed the name of verifyHash to verify since Im checking the pass against a hash and not just verifying a hash.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const bcrypt = require('./helpers/bcrypt');
 async function test() {
     const testHash = await bcrypt.hashPassword('testing');
     console.log(`testhash: ${testHash}`);
-    console.log(await bcrypt.verifyHash('testing', testHash));
+    console.log(await bcrypt.verify('testing', testHash));
 }
 
 test();
