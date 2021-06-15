8ca5db00e3fd0e451219432a81d7763e612ac0d6
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-15 15:46:20 -0700

    Local authorization endpoint is now working. It returns the JWT signed to the user_id if it succeeds. It lets the user know if theres an email unique constraint. Catches any errors with a generic 500 status code and ensures the req.body has the proper items attached.

--- db_helpers/users_table.js
+++ db_helpers/users_table.js
@@ -1,15 +1,12 @@
 const db = require('./config_db');
 
-async function createTable() {
-    
-}
-
 async function truncateTable() {
     try {
         return await db.query('TRUNCATE TABLE users RESTART IDENTITY CASCADE');
     }
     catch(err) {
         console.error(err.stack);
+        return err;
     }
 }
 
@@ -18,27 +15,31 @@ async function insert(user) {
         return await db.query(`INSERT INTO users(email, hash) VALUES($1, $2) RETURNING  user_id, email`, [user.email, user.hash]);
     }
     catch(err) {
-        console.error(err.stack);
+        return err;
     }
 }
 
-async function selectById(id) {
+async function selectByID(id) {
     try {
-        return await db.query('SELECT * FROM users WHERE user_id=$1', id);
+        return await db.query('SELECT * FROM users WHERE user_id=$1', [id]);
     }
     catch(err) {
-        console.error(err.stack);
+        return err;
     }
 }
 
 async function selectByEmail(email) {
-    
+    try {
+        return await db.query('SELECT * FROM users WHERE email=$1', [email]);
+    }
+    catch(err) {
+        return err;
+    }
 }
 
 module.exports = {
-    createTable,
     truncateTable,
     insert,
-    selectById,
+    selectByID,
     selectByEmail,
 }
