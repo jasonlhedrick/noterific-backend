230088061ef72b4a8f067632f29d5ec6455172e1
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-15 14:35:08 -0700

    Having a lot of JEST failures in parsing files despite using all CommonJS conventions. Have to figure it out.

--- tests/userDB.test.js
+++ tests/userDB.test.js
@@ -1,5 +1,14 @@
 const users = require('../db_helpers/users_table');
+const bcrypt = require('../helpers/bcrypt');
+const jwt = require('../helpers/jwt');
 
 afterAll(async () => {
    return await users.truncateTable();
+});
+
+describe('Testing the users table.', () => {
+    const hash = await bcrypt('password');
+    const email = 'test@jest.com';
+    const user = await users.insert({email: email, hash: hash});
+    expect(user.rows[0].user_id).toBe(1);
 });
