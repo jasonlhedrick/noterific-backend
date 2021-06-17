0b08927adcfc2813b6703127ac0006fbfe0c9309
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-17 16:11:42 -0700

    Added delete and update operations to the notes_table helpers. Created the endpoint handling for updating and deleting a note. Ensured everything was working with Insomnia API checker.

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -12,15 +12,51 @@ notes.post('/', verify, async function (req, res) {
     const note = {title: req.body.title, body: req.body.body}
     const userID = req.body.userID;
     const updatedRow = await notesTable.add(note, userID)
-    res.status(200).json(updatedRow.rows);
+    res.status(201).json(updatedRow.rows);
 });
 
 notes.put('/:note_id', verify, async function (req, res) {
-    res.status(200).json({message: 'Under construction.'});
+    const note = {title: req.body.title, body: req.body.body, id: req.params.note_id}
+    const userID = req.body.userID;
+
+    if (note.title && note.body) {
+        const checkNoteUserID = await notesTable.selectByNoteID(note.id);
+
+        // Ensure this note ID is owned by the session token's userID.
+        if (checkNoteUserID.rows[0].user_id.toString() === userID) {
+            const updatedNote = await notesTable.updateByNoteID(note);
+            if (updatedNote.command === 'UPDATE') {
+                res.status(200).json({message: 'Note updated successfully.', note_id: note.id});
+            }
+        }
+        else {
+            res.status(403).json({message: `Note id: ${note.id} is not owned by current user.`});
+        }
+    }
+    else {
+        res.status(400).json({message: 'Updating a note requires a title and body.',});
+    }
 });
 
 notes.delete('/:note_id', verify, async function(req, res) {
-    res.status(200).json({message: 'Under construction.'});
+    const noteID = req.params.note_id
+    const userID = req.body.userID;
+    const checkNoteUserID = await notesTable.selectByNoteID(noteID);
+    if (checkNoteUserID.rows.length === 0) {
+        res.status(410).json({message: `Note id: ${noteID} not found.`});
+    }
+    else {
+        if (checkNoteUserID.rows[0].user_id.toString() === userID) {
+            const deleteNote = await notesTable.deleteByNoteID(noteID);
+            console.log(deleteNote);
+            if (deleteNote.command === 'DELETE') {
+                res.status(200).json({message: `Note id: ${noteID} successfully deleted.`});
+            }
+        }
+        else {
+            res.status(403).json({message: `Note id: ${noteID} is not owned by current user.`});
+        }
+    }
 });
 
 module.exports = notes;
