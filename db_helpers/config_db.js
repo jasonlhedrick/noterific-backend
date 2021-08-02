083a5a69d3bb59015938d032973d5024b60ed1a7
Jason Hedrick <jasonleehedrick@gmail.com>
2021-08-01 20:53:57 -0700

    heroku.

--- db_helpers/config_db.js
+++ db_helpers/config_db.js
@@ -3,9 +3,10 @@ const { Pool } = require('pg');
 const db = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
+    database: process.env.DATABASE_URL,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
 });
 
+console.log(db);
 module.exports = db;
